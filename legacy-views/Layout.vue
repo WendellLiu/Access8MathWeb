93416b92343510bcc94bd1f5f3aa6eb6d66493b1
Wendell Liu <cuk.bas@gmail.com>
2023-10-02 19:56:27 +0800

    chore: load legacy code

---
+++ legacy-views/Layout.vue
@@ -0,0 +1,16 @@
+﻿<template>
+	<div>
+		<AppHeader />
+		<main class="container" id="main_id"><router-view /></main>
+	</div>
+</template>
+
+<script>
+import AppHeader from "./AppHeader.vue";
+export default {
+	name: "Layout",
+	components: {
+		AppHeader,
+	},
+};
+</script>

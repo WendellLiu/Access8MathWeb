da30c0aec3fecbcdffd8310b25ac53058d8c54ac
Wendell Liu <cuk.bas@gmail.com>
2023-10-05 23:30:47 +0800

    feat: upper greek alphabet tabs

--- src/lib/tabs/math/upper-greek-alphabet.js
+++ src/lib/tabs/math/upper-greek-alphabet.js
@@ -1,4 +1,27 @@
 import { ReactComponent as AlphaUpper } from '@/components/svg/upper-greek-alphabet/alpha-upper.svg';
+import { ReactComponent as BetaUpper } from '@/components/svg/upper-greek-alphabet/beta-upper.svg';
+import { ReactComponent as ChiUpper } from '@/components/svg/upper-greek-alphabet/chi-upper.svg';
+import { ReactComponent as DeltaUpper } from '@/components/svg/upper-greek-alphabet/delta-upper.svg';
+import { ReactComponent as EpsilonUpper } from '@/components/svg/upper-greek-alphabet/epsilon-upper.svg';
+import { ReactComponent as EtaUpper } from '@/components/svg/upper-greek-alphabet/eta-upper.svg';
+import { ReactComponent as GammaUpper } from '@/components/svg/upper-greek-alphabet/gamma-upper.svg';
+import { ReactComponent as IotaUpper } from '@/components/svg/upper-greek-alphabet/iota-upper.svg';
+import { ReactComponent as KappaUpper } from '@/components/svg/upper-greek-alphabet/kappa-upper.svg';
+import { ReactComponent as LambdaUpper } from '@/components/svg/upper-greek-alphabet/lambda-upper.svg';
+import { ReactComponent as MuUpper } from '@/components/svg/upper-greek-alphabet/mu-upper.svg';
+import { ReactComponent as NuUpper } from '@/components/svg/upper-greek-alphabet/nu-upper.svg';
+import { ReactComponent as OmegaUpper } from '@/components/svg/upper-greek-alphabet/omega-upper.svg';
+import { ReactComponent as omicronUpper } from '@/components/svg/upper-greek-alphabet/omicron-upper.svg';
+import { ReactComponent as PhiUpper } from '@/components/svg/upper-greek-alphabet/phi-upper.svg';
+import { ReactComponent as PiUpper } from '@/components/svg/upper-greek-alphabet/pi-upper.svg';
+import { ReactComponent as PsiUpper } from '@/components/svg/upper-greek-alphabet/psi-upper.svg';
+import { ReactComponent as RhoUpper } from '@/components/svg/upper-greek-alphabet/rho-upper.svg';
+import { ReactComponent as SigmaUpper } from '@/components/svg/upper-greek-alphabet/sigma-upper.svg';
+import { ReactComponent as TauUpper } from '@/components/svg/upper-greek-alphabet/tau-upper.svg';
+import { ReactComponent as ThetaUpper } from '@/components/svg/upper-greek-alphabet/theta-upper.svg';
+import { ReactComponent as UpsilonUpper } from '@/components/svg/upper-greek-alphabet/upsilon-upper.svg';
+import { ReactComponent as XiUpper } from '@/components/svg/upper-greek-alphabet/xi-upper.svg';
+import { ReactComponent as ZetaUpper } from '@/components/svg/upper-greek-alphabet/zeta-upper.svg';
 
 const upperGreekAlphabet = [
   {
@@ -8,6 +31,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'A',
     order: 0,
+    Icon: AlphaUpper,
   },
   {
     id: 'beta-upper',
@@ -16,6 +40,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'B',
     order: 1,
+    Icon: BetaUpper,
   },
   {
     id: 'theta-upper',
@@ -24,6 +49,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'C',
     order: 7,
+    Icon: ThetaUpper,
   },
   {
     id: 'delta-upper',
@@ -32,6 +58,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'D',
     order: 3,
+    Icon: DeltaUpper,
   },
   {
     id: 'epsilon-upper',
@@ -40,6 +67,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'E',
     order: 4,
+    Icon: EpsilonUpper,
   },
   {
     id: 'phi-upper',
@@ -48,6 +76,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'F',
     order: 20,
+    Icon: PhiUpper,
   },
   {
     id: 'gamma-upper',
@@ -56,6 +85,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'G',
     order: 2,
+    Icon: GammaUpper,
   },
   {
     id: 'eta-upper',
@@ -64,6 +94,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'H',
     order: 6,
+    Icon: EtaUpper,
   },
   {
     id: 'iota-upper',
@@ -72,6 +103,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'I',
     order: 8,
+    Icon: IotaUpper,
   },
   {
     id: 'kappa-upper',
@@ -80,6 +112,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'K',
     order: 9,
+    Icon: KappaUpper,
   },
   {
     id: 'lambda-upper',
@@ -88,6 +121,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'L',
     order: 10,
+    Icon: LambdaUpper,
   },
   {
     id: 'mu-upper',
@@ -96,6 +130,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'M',
     order: 11,
+    Icon: MuUpper,
   },
   {
     id: 'nu-upper',
@@ -104,6 +139,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'N',
     order: 12,
+    Icon: NuUpper,
   },
   {
     id: 'omicron-upper',
@@ -112,6 +148,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'O',
     order: 14,
+    Icon: omicronUpper,
   },
   {
     id: 'pi-upper',
@@ -120,6 +157,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'P',
     order: 15,
+    Icon: PiUpper,
   },
   {
     id: 'rho-upper',
@@ -128,6 +166,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'R',
     order: 16,
+    Icon: RhoUpper,
   },
   {
     id: 'sigma-upper',
@@ -136,6 +175,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'S',
     order: 17,
+    Icon: SigmaUpper,
   },
   {
     id: 'tau-upper',
@@ -144,6 +184,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'T',
     order: 18,
+    Icon: TauUpper,
   },
   {
     id: 'upsilon-upper',
@@ -152,6 +193,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'U',
     order: 19,
+    Icon: UpsilonUpper,
   },
   {
     id: 'psi-upper',
@@ -160,6 +202,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'V',
     order: 22,
+    Icon: PsiUpper,
   },
   {
     id: 'omega-upper',
@@ -168,6 +211,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'W',
     order: 23,
+    Icon: OmegaUpper,
   },
   {
     id: 'xi-upper',
@@ -176,6 +220,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'Y',
     order: 13,
+    Icon: XiUpper,
   },
   {
     id: 'chi-upper',
@@ -184,6 +229,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'X',
     order: 21,
+    Icon: ChiUpper,
   },
   {
     id: 'zeta-upper',
@@ -192,6 +238,7 @@ const upperGreekAlphabet = [
     category: 'upper_greek_alphabet',
     shortcut: 'Z',
     order: 5,
+    Icon: ZetaUpper,
   },
 ];
 

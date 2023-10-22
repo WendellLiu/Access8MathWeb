4287f4674e11a9cd1ce77b6c53c32c969b4ea4ca
Wendell Liu <cuk.bas@gmail.com>
2023-10-22 18:47:19 +0800

    feat: main

--- src/pages/home.js
+++ src/pages/home.js
@@ -263,110 +263,115 @@ export default function Home() {
   const t = useTranslation('home');
 
   return (
-    <div className="home flex flex-wrap pt-16 md:pt-p2 h-screen w-screen overflow-x-hidden overflow-y-auto">
-      {/* Left side input panel */}
-      <div className="w-2/4 p-p1 flex-basis-like-1/1 grow md:flex-1 bg-bg1 text-left pt-p1 pl-p3 pr-p3 flex flex-col">
-        <p role="heading" aria-level="1" className="text-2xl md:text-3xl">
-          {t('editContent')}
-        </p>
-        <div className="flex justify-end mb-4 mt-8 md:mt-m1">
-          <Button variant="primary" className="ml-2" onClick={insertMark}>
-            {t('mark')}
-          </Button>
-          <Button
-            variant="primary"
-            className="ml-2"
-            onClick={() => laTeXSepConvert('d2b')}
-          >
-            {t('dollar2bracket')}
-          </Button>
-          <Button
-            variant="primary"
-            className="ml-2"
-            onClick={() => laTeXSepConvert('b2d')}
-          >
-            {t('bracket2dollar')}
-          </Button>
-          <Button variant="primary" className="ml-2" onClick={importClick}>
-            {t('import')}
-          </Button>
-          <Button variant="primary" className="ml-2" onClick={exportClick}>
-            {t('export')}
-          </Button>
-          <button
-            className="hover:scale-110 transition-scale ml-2"
-            onClick={() => setShowTipModal(true)}
-            aria-label={t('descript')}
-          >
-            <QuestionCircleComponent />
-          </button>
+    <main className="container">
+      <div className="home flex flex-wrap pt-16 md:pt-20 h-screen w-screen overflow-x-hidden overflow-y-auto">
+        {/* Left side input panel */}
+        <div className="w-2/4 p-3 flex-basis-like-1/1 grow md:flex-1 bg-bg1 text-left pt-3 pl-8 pr-8 flex flex-col">
+          <p role="heading" aria-level="1" className="text-2xl md:text-3xl">
+            {t('editContent')}
+          </p>
+          <div className="flex justify-end mb-4 mt-8 md:mt-m1">
+            <Button variant="primary" className="ml-2" onClick={insertMark}>
+              {t('mark')}
+            </Button>
+            <Button
+              variant="primary"
+              className="ml-2"
+              onClick={() => laTeXSepConvert('d2b')}
+            >
+              {t('dollar2bracket')}
+            </Button>
+            <Button
+              variant="primary"
+              className="ml-2"
+              onClick={() => laTeXSepConvert('b2d')}
+            >
+              {t('bracket2dollar')}
+            </Button>
+            <Button variant="primary" className="ml-2" onClick={importClick}>
+              {t('import')}
+            </Button>
+            <Button variant="primary" className="ml-2" onClick={exportClick}>
+              {t('export')}
+            </Button>
+            <button
+              className="hover:scale-110 transition-scale ml-2"
+              onClick={() => setShowTipModal(true)}
+              aria-label={t('descript')}
+            >
+              <QuestionCircleComponent />
+            </button>
+          </div>
+          <EditIconsTab insertLatex={insertLatex} />
+          <div className="flex flex-1">
+            {basic ? (
+              <textarea
+                ref={inputArea}
+                className="left-side-input-textarea flex-1 resize-none p-3 border border-bd1 overflow-y-scroll rounded-b-lg"
+                type="text"
+                value={data}
+                onChange={(e) => {
+                  setData(e.target.value);
+                }}
+              />
+            ) : (
+              <div
+                id="codemirror"
+                className="left-side-input-textarea flex-1 resize-none border border-bd1 overflow-y-scroll rounded-b-lg"
+              ></div>
+            )}
+            <input
+              ref={importFile}
+              type="file"
+              className="hidden"
+              onChange={importAction}
+            />
+          </div>
         </div>
-        <EditIconsTab insertLatex={insertLatex} />
-        <div className="flex flex-1">
-          {basic ? (
-            <textarea
-              ref={inputArea}
-              className="left-side-input-textarea flex-1 resize-none p-3 border border-bd1 overflow-y-scroll rounded-b-lg"
-              type="text"
-              value={data}
-              onChange={(e) => {
-                setData(e.target.value);
-              }}
+
+        {/* Right side output panel */}
+        <div className="w-2/4 flex-basis-like-1/1 grow md:flex-1 text-left p-p1 pt-p1 pl-p3 flex flex-col">
+          <div className="flex md:mb-m3 w-100 justify-between">
+            <p
+              role="heading"
+              aria-level="1"
+              className="text-2xl md:text-3xl w-100"
+            >
+              {t('result')}
+            </p>
+            <button
+              onClick={() => setShowSettingModal(true)}
+              aria-label={t('setting')}
+            >
+              <SettingComponent />
+            </button>
+          </div>
+          {displayConfig.htmlDocumentDisplay === 'markdown' ? (
+            <div
+              className="right-side-input-textarea border-2 overflow-scroll p-4 flex-1 rounded-lg"
+              dangerouslySetInnerHTML={{ __html: contentmd }}
             />
           ) : (
-            <div
-              id="codemirror"
-              className="left-side-input-textarea flex-1 resize-none border border-bd1 overflow-y-scroll rounded-b-lg"
-            ></div>
+            <div className="right-side-input-textarea border-2 overflow-scroll p-4 flex-1 rounded-lg">
+              {content.map((line, key) => (
+                <span key={key}>
+                  <span dangerouslySetInnerHTML={{ __html: line }} />
+                </span>
+              ))}
+            </div>
           )}
-          <input
-            ref={importFile}
-            type="file"
-            className="hidden"
-            onChange={importAction}
-          />
         </div>
+        <TipModal
+          isOpen={showTipModal}
+          onClose={() => setShowTipModal(false)}
+        />
+        <SettingModal
+          isOpen={showSettingModal}
+          onClose={() => setShowSettingModal(false)}
+          onSubmit={setDisplayConfig}
+          displayConfig={displayConfig}
+        />
       </div>
-
-      {/* Right side output panel */}
-      <div className="w-2/4 flex-basis-like-1/1 grow md:flex-1 text-left p-p1 pt-p1 pl-p3 flex flex-col">
-        <div className="flex md:mb-m3 w-100 justify-between">
-          <p
-            role="heading"
-            aria-level="1"
-            className="text-2xl md:text-3xl w-100"
-          >
-            {t('result')}
-          </p>
-          <button
-            onClick={() => setShowSettingModal(true)}
-            aria-label={t('setting')}
-          >
-            <SettingComponent />
-          </button>
-        </div>
-        {displayConfig.htmlDocumentDisplay === 'markdown' ? (
-          <div
-            className="right-side-input-textarea border-2 overflow-scroll p-4 flex-1 rounded-lg"
-            dangerouslySetInnerHTML={{ __html: contentmd }}
-          />
-        ) : (
-          <div className="right-side-input-textarea border-2 overflow-scroll p-4 flex-1 rounded-lg">
-            {content.map((line, key) => (
-              <span key={key}>
-                <span dangerouslySetInnerHTML={{ __html: line }} />
-              </span>
-            ))}
-          </div>
-        )}
-      </div>
-      <TipModal isOpen={showTipModal} onClose={() => setShowTipModal(false)} />
-      <SettingModal
-        isOpen={showSettingModal}
-        onClose={() => setShowSettingModal(false)}
-        onSubmit={setDisplayConfig}
-        displayConfig={displayConfig}
-      />
-    </div>
+    </main>
   );
 }

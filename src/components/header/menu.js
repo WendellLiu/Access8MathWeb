8fedb3c64fbf918acde92fe65c983cc0e1257376
Wendell Liu <cuk.bas@gmail.com>
2023-11-01 22:57:50 +0800

    feat: active menu item

--- src/components/header/menu.js
+++ src/components/header/menu.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useMemo } from 'react';
-import { Popover, Transition } from '@headlessui/react';
+import { Transition, Menu } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
 import { useTranslation } from '@/lib/i18n';
@@ -38,43 +38,55 @@ const NativeMenu = () => {
   }, [t]);
 
   return (
-    <Popover className="relative">
-      <Popover.Button className="flex items-center md:text-xl text-base font-semibold leading-8 text-gray-900">
-        <span>{t('more')}</span>
-        <ChevronDownIcon
-          className="h-5 w-5 flex-none text-gray-400 ml-1"
-          aria-hidden="true"
-        />
-      </Popover.Button>
+    <Menu as="div" className="relative">
+      <div>
+        <Menu.Button
+          className="flex items-center md:text-xl text-base font-semibold leading-8 text-gray-900"
+          aria-label={t('more')}
+        >
+          <span>{t('more')}</span>
+          <ChevronDownIcon
+            className="h-5 w-5 flex-none text-gray-400 ml-1"
+            aria-hidden="true"
+          />
+        </Menu.Button>
+      </div>
 
       <Transition
         as={Fragment}
-        enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
       >
-        <Popover.Panel className="absolute right-0 z-10 mt-5 flex w-screen max-w-max">
+        <Menu.Items className="absolute right-0 z-10 mt-5 flex w-screen max-w-max">
           <div className="overflow-hidden rounded-xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
             <div>
               {items.map(({ id, name, href }) => (
-                <a
-                  key={id}
-                  href={href}
-                  className="font-semibold text-gray-900 group relative flex rounded-lg p-4 hover:bg-gray-50"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {name}
-                </a>
+                <Menu.Item key={id}>
+                  {({ active }) => {
+                    return (
+                      <a
+                        href={href}
+                        className={`${
+                          active ? 'font-bold' : 'font-normal'
+                        } text-gray-900 group relative flex rounded-lg p-4 hover:bg-gray-50`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {name}
+                      </a>
+                    );
+                  }}
+                </Menu.Item>
               ))}
             </div>
           </div>
-        </Popover.Panel>
+        </Menu.Items>
       </Transition>
-    </Popover>
+    </Menu>
   );
 };
 

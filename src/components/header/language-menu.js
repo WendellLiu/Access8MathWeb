f03e897a888cdea37e0e11939167f9021de8f548
Wendell Liu <cuk.bas@gmail.com>
2023-11-01 23:09:01 +0800

    feat: language menu

--- src/components/header/language-menu.js
+++ src/components/header/language-menu.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
+import { Menu, Transition } from '@headlessui/react';
 import { LanguageIcon } from '@heroicons/react/20/solid';
 
 import i18n, { useTranslation } from '@/lib/i18n';
@@ -12,41 +12,59 @@ const LANGUAGES = [
 ];
 
 const LanguageMenu = () => {
-  const commonT = useTranslation('common');
+  const t = useTranslation('common');
 
   return (
-    <Popover className="relative">
-      <Popover.Button className="flex items-center md:text-xl text-base font-semibold leading-8 text-gray-900">
-        <LanguageIcon className="h-5 w-5 flex-none mr-1" aria-hidden="true" />
-        <span>{commonT(`locale.${i18n.language}`)}</span>
-      </Popover.Button>
+    <Menu as="div" className="relative">
+      {({ open }) => (
+        <>
+          <Menu.Button
+            className="flex items-center md:text-xl text-base font-semibold leading-8 text-gray-900"
+            aria-label={t('locale.${i18n.language}')}
+          >
+            <LanguageIcon
+              className="h-5 w-5 flex-none mr-1"
+              aria-hidden="true"
+            />
+            <span>{t(`locale.${i18n.language}`)}</span>
+          </Menu.Button>
+          {open && <div className="fixed z-10 inset-0 bg-black opacity-30" />}
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
-      >
-        <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-          <div className="overflow-hidden rounded-xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-            <div>
-              {LANGUAGES.map(({ locale }) => (
-                <button
-                  key={locale}
-                  className="group relative flex rounded-lg p-4 hover:bg-gray-50 font-semibold text-gray-900 w-full"
-                  onClick={() => i18n.changeLanguage(locale)}
-                >
-                  {commonT(`locale.${locale}`)}
-                </button>
-              ))}
-            </div>
-          </div>
-        </Popover.Panel>
-      </Transition>
-    </Popover>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-5 flex w-screen max-w-max">
+              <div className="overflow-hidden rounded-xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+                <div>
+                  {LANGUAGES.map(({ locale }) => (
+                    <Menu.Item key={locale}>
+                      {({ active }) => {
+                        return (
+                          <button
+                            className={`group relative flex rounded-lg p-4 hover:bg-gray-50 text-gray-900 w-full ${
+                              active ? 'font-bold' : 'font-normal'
+                            }`}
+                            onClick={() => i18n.changeLanguage(locale)}
+                          >
+                            {t(`locale.${locale}`)}
+                          </button>
+                        );
+                      }}
+                    </Menu.Item>
+                  ))}
+                </div>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
   );
 };
 
